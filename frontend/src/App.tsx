038443f2f33dfdc6fd4f7a7953400010f575c3d6
squid-cat <kaikai120626@gmail.com>
2025-12-31 13:39:29 +0900

    fix: ルーティングからコントローラーを削除

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NotFound } from "./pages/NotFound";
-import { TrainController } from "./pages/TrainController";
 import { UnityPage } from "./pages/UnityPage";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<UnityPage />} />
-        <Route path="/controller/:roomId" element={<TrainController />} />
         <Route path="/notfound" element={<NotFound />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
